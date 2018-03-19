5e892e36fbb2ed8a6c9bf950c6f518674ba40a6a
Michael Ordaz <ordazmartinezmichael@gmail.com>
2018-03-19 15:33:06 -0600

    Agregado char js

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -10,9 +10,13 @@ $(function(){
 
 		if(power >= 12){
 			swal("Poder Mecanico Peligroso!!", "nivel mecanico de "+ power, "error");
+			alertify.error("Poder Mecanico Peligroso!!");
+
 		}
 		else if(power < 12){
 			swal("Poder Mecanico Estable!!", "nivel mecanico de "+ power, "success");
+			alertify.success("Poder Mecanico Estable!!");
+
 		}
 
 	});
@@ -23,7 +27,43 @@ $(function(){
 		parte1 = pico - parte1;
 		var parte2 = (fr * vt) / 1000;
 		var result = cmAgua * parte2 * parte1;
+
+	var ctx = document.querySelector("#myGraf");
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ["Poder Mecanico", "Blue", "Yellow", "Green"],
+	        datasets: [{
+	            label: 'Ventilación Humana',
+	            data: [result, 19, 3, 5],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+
 		return parseInt( result );
 	}
+
 
 })
